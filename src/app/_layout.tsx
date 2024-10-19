3ac66e8f2e27ededc24a505af60d3529a8226dcd
Muhammad Talha Khan <mtalha@kodexlabs.com>
2024-10-20 03:14:07 +0500

    Gave title to the products page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -57,7 +57,7 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-        <Stack.Screen name="productsPage" />
+        <Stack.Screen name="productsPage" options={{ title: "Products" }} />
       </Stack>
     </ThemeProvider>
   );
